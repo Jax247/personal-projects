b917ef3f089372d69115207240b7f719d4855267
Jay  Jeffries <jewan.jeffries@gmail.com>
2021-07-18 01:39:32 -0400

    start projects file to import into portfolio

---
+++ portfolio/js/projects.js
@@ -0,0 +1,32 @@
+const projects = [
+    { 
+        name: 'Millenia Weather',
+        type: 'React App',
+        tags: ['React', 'Styled Components'],
+        screenshotpath: '../assets/Capture.PNG',
+        link: ''
+    },
+    { 
+        name: 'Portfolio',
+        type: 'Personal Portfolio',
+        tags: ['HTML', 'CSS', 'Sass','JQuery'],
+        screenshotpath: '',
+        link: ''
+    },
+    { 
+        name: 'Hax.io',
+        type: 'Standard Site Example',
+        tags: ['HTML', 'CSS'],
+        screenshotpath: '',
+        link: ''
+    },
+    {
+        name: 'LitFit',
+        type: 'Login Page Example',
+        tags: ['HTML', 'CSS'],
+        screenshotpath: '',
+        link: ''
+    }
+]
+
+export default projects
