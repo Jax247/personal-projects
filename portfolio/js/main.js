41ead5f74c88cc4abdc3989522d5be0b7bfb02bb
Jay  Jeffries <jewan.jeffries@gmail.com>
2021-07-22 22:06:10 -0400

    Project Data rendered to ui

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,19 +1,79 @@
+generateProjectCards();
+
 $(function () {
-  $("#myBtn").click(function () {
-    $(".modal").css('display', 'flex')
+  $(".project-card").click(function () {
+    generateModal(this);
   });
+
   $(".close").click(function () {
-    $(".modal").css('display', 'none')
+    closeModal();
   });
-
-
-  
 });
 
-// When the user clicks anywhere outside of the modal, close it
-
-window.onclick = function (event) {
-  if (event.target == $(".modal")) {
-    $(".modal").css('display', 'none')
+$(window).click(function (e) {
+  if (e.target == $("#project-modal")) {
+    closeModal();
   }
-};
+});
+
+// Populate modal with data relevant to the project being passed in
+function generateModal(e) {
+  // get relevant divs to change data
+  console.log(e);
+
+  let project = projects.filter((p) => p.name === e.id)[0]; // comes back as array
+
+  console.log(project);
+  $("#modal-title").html(`${project.name} - ${project.type}`);
+  $("#project-description").html(project.description);
+  $("#project-demo").attr("src", `${project.screenshotpath}`)
+
+  project.tags.forEach((tag) => {
+    $("#tag-list").append(`<li>
+                                <img src="./assets/tick.png" alt="check"/>
+                                ${tag}
+                            </li>`);
+  });
+  $(".modal").css("display", "flex");
+}
+
+function closeModal() {
+
+    $("#tag-list").empty();
+    $(".modal").css("display", "none");
+}
+
+function generateProjectCards(e) {
+  let cardList = [];
+  let card;
+  let img;
+  let overlay;
+  let icon;
+
+  projects.forEach((project) => {
+    // create card container
+    card = document.createElement("div");
+    card.className = "project-card";
+    card.id = project.name;
+
+    img = document.createElement("img");
+    img.src = project.screenshotpath;
+
+    overlay = document.createElement("div");
+    overlay.className = "overlay";
+
+    icon = document.createElement("img");
+    icon.className = "zoom-icon";
+    icon.src = "./assets/pngaaa.com-3712648.png";
+
+    card.appendChild(img);
+    card.appendChild(overlay);
+    overlay.appendChild(icon);
+
+    cardList.push(card);
+  });
+
+  cardList.forEach((card) => {
+    $("#project-grid").append(card);
+  });
+}
--- portfolio/js/projects.js
+++ portfolio/js/projects.js
@@ -1,32 +1,36 @@
 const projects = [
-    { 
-        name: 'Millenia Weather',
-        type: 'React App',
-        tags: ['React', 'Styled Components', 'External API'],
-        screenshotpath: '../assets/Capture.PNG',
-        link: ''
-    },
-    { 
-        name: 'Portfolio',
-        type: 'Personal Portfolio',
-        tags: ['HTML', 'CSS', 'Sass','JQuery'],
-        screenshotpath: '',
-        link: ''
-    },
-    { 
-        name: 'Hax.io',
-        type: 'Standard Site Example',
-        tags: ['HTML', 'CSS'],
-        screenshotpath: '',
-        link: ''
-    },
-    {
-        name: 'LitFit',
-        type: 'Login Page Example',
-        tags: ['HTML', 'CSS'],
-        screenshotpath: '',
-        link: ''
-    }
-]
+  {
+    name: "Millenia Weather",
+    type: "React App",
+    tags: ["React", "CSS", "External API"],
+    screenshotpath: "./assets/Capture.PNG",
+    description: "Weather App made With React and Accuweather API",
+    link: "#",
+  },
+  {
+    name: "Portfolio",
+    type: "Personal Portfolio",
+    tags: ["HTML", "CSS", "Sass", "JQuery"],
+    screenshotpath: "./assets/Capture.PNG",
+    description: "Weather App made With React and Accuweather API",
+    link: "#",
+  },
+  {
+    name: "Hax",
+    type: "Standard Site Example",
+    tags: ["HTML", "CSS"],
+    screenshotpath: "./assets/Capture.PNG",
+    description: "Weather App made With React and Accuweather API",
+    link: "#",
+  },
+  {
+    name: "LitFit",
+    type: "Login Page Example",
+    tags: ["HTML", "CSS"],
+    screenshotpath: "./assets/Capture.PNG",
+    description: "Weather App made With React and Accuweather API",
+    link: "#",
+  },
+];
 
-export default projects
+// module.exports = projects
