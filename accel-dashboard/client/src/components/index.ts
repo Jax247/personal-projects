b43ba58e22661220f9912e88789369443a890b78
Jay  Jeffries <jewan.jeffries@gmail.com>
2023-02-01 12:59:58 -0500

    components export

---
+++ accel-dashboard/client/src/components/index.ts
@@ -0,0 +1,8 @@
+import Header from "./Header";
+import FlexBetween from "./FlexBetween";
+import NavBar from "./global/NavBar";
+import SideBar from "./global/SideBar";
+import CustomDGToolbar from "./CustomDGToolbar";
+import OverviewLineChart from "./OverviewLineChart";
+
+export { Header, FlexBetween, NavBar, SideBar, CustomDGToolbar, OverviewLineChart };
