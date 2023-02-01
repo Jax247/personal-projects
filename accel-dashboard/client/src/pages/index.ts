7fb9ce46e75140e558f4d024422f3808e1b0d05d
Jay  Jeffries <jewan.jeffries@gmail.com>
2023-02-01 13:07:56 -0500

    Create index.ts

---
+++ accel-dashboard/client/src/pages/index.ts
@@ -0,0 +1,25 @@
+import Dashboard from "./Dashboard/";
+import Customers from "./Customers";
+import Transactions from "./Transactions";
+import AddContact from "./Form/AddContact";
+import FAQ from "./FAQ";
+import Layout from "./Layout/Layout";
+import Products from "./Products";
+import Geography from "./Geography";
+import Overview from "./Overview";
+import Daily from "./Daily";
+import Monthly from "./Monthly";
+
+export {
+  Dashboard,
+  Products,
+  Customers,
+  Transactions,
+  AddContact,
+  FAQ,
+  Geography,
+  Overview,
+  Layout,
+  Daily,
+  Monthly
+};
