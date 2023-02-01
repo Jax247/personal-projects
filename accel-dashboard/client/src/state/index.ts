3b0a93d6cc5ab76a80258ba441d1e4707eb74051
Jay  Jeffries <jewan.jeffries@gmail.com>
2023-02-01 12:57:51 -0500

    Configure RTK store

---
+++ accel-dashboard/client/src/state/index.ts
@@ -0,0 +1,51 @@
+import {
+  createSlice,
+  CreateSliceOptions,
+  configureStore,
+} from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { Api } from "./api";
+
+
+interface ModeState {
+    mode: string;
+    user: string;
+}
+
+const initialState: ModeState = {
+  mode: "dark",
+  user: "63701cc1f03239db6900011f"
+};
+
+const sliceOptions = {
+  name: "global",
+  initialState,
+  reducers: {
+    setMode: (state: ModeState) => {
+       state.mode = state.mode === "light" ? "dark" : "light"
+    },
+  },
+} as CreateSliceOptions;
+
+export const globalSlice = createSlice(sliceOptions);
+
+export const setMode  = globalSlice.actions;
+
+
+
+const store = configureStore({
+  reducer: {
+    global: globalSlice.reducer,
+    [Api.reducerPath] : Api.reducer,
+},
+middleware: (getDefault) => getDefault().concat(Api.middleware)
+});
+
+setupListeners(store.dispatch)
+
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
