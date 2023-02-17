6a539296d0ebd4cfa01c43208c85a0d350698874
Jay  Jeffries <jewan.jeffries@gmail.com>
2023-02-16 20:01:08 -0500

    Admin Page Added

---
+++ accel-dashboard/client/src/pages/AdminList/index.tsx
@@ -0,0 +1,101 @@
+import { Box, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import React from "react";
+import { CustomDGMenu, Header } from "../../components";
+import Loader from "../../Loader";
+import { useGetAdminsQuery } from "../../state/api";
+
+const AdminList = () => {
+  const theme = useTheme()
+  const { data, isLoading } = useGetAdminsQuery("User");
+
+  if (isLoading) {
+    return <Loader/>
+  }
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 0.5,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "phoneNumber",
+      headerName: "Phone Number",
+      flex: 0.5,
+      renderCell: (params:any) => {
+        return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3");
+      },
+    },
+    {
+      field: "country",
+      headerName: "Country",
+      flex: 0.4,
+    },
+    {
+      field: "occupation",
+      headerName: "Occupation",
+      flex: 1,
+    },
+    {
+      field: "role",
+      headerName: "Role",
+      flex: 0.5,
+    },
+  ];
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="ADMINS" subtitle="Managing admins and list of admins" />
+      <Box
+        mt="40px"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.paper,
+            color: theme.palette.secondary.light,
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.paper,
+            color: theme.palette.secondary.light,
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary.light} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={data || []}
+          columns={columns}
+          components={{
+            ColumnMenu: CustomDGMenu,
+          }}
+        />
+      </Box>
+    </Box>
+  )
+};
+
+export default AdminList;
