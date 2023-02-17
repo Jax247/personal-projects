000db691b94d0397fcd45c52e8806d5dca0e2bcd
Jay  Jeffries <jewan.jeffries@gmail.com>
2023-02-16 20:01:49 -0500

    Daily Chart changes

--- accel-dashboard/client/src/pages/Daily/index.tsx
+++ accel-dashboard/client/src/pages/Daily/index.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo, useState } from "react";
-import { Box, useTheme } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import { Header } from "../../components";
 import Loader from "../../Loader";
 import { Datum, ResponsiveLine, Serie } from "@nivo/line";
@@ -64,11 +64,20 @@ const Daily = () => {
   }
 
   return (
-    <Box>
+    <Box sx={{ p: "1rem", pb: '100px;' }}>
       <Header title={` Daily `} subtitle=" " />
       <Box height="80vh">
-        <Box display="flex" justifyContent="flex-end">
+        <Box display="flex" gap="30px" justifyContent="flex-start">
           <Box>
+            <Typography
+              sx={{
+                color: theme.palette.secondary.main,
+                textAlign: "center",
+                mb: "1rem",
+              }}
+            >
+              From
+            </Typography>
             <DatePicker
               selected={startDate}
               onChange={(date: Date) => setStartDate(date)}
@@ -78,6 +87,15 @@ const Daily = () => {
             />
           </Box>
           <Box>
+            <Typography
+              sx={{
+                color: theme.palette.secondary.main,
+                textAlign: "center",
+                mb: "1rem",
+              }}
+            >
+              To
+            </Typography>
             <DatePicker
               selected={endDate}
               onChange={(date: Date) => setEndDate(date)}
@@ -170,109 +188,7 @@ const Daily = () => {
               anchor: "top-right",
               direction: "column",
               justify: false,
-              translateX: 50,
-              translateY: 0,
-              itemsSpacing: 0,
-              itemDirection: "left-to-right",
-              itemWidth: 80,
-              itemHeight: 20,
-              itemOpacity: 0.75,
-              symbolSize: 12,
-              symbolShape: "circle",
-              symbolBorderColor: "rgba(0, 0, 0, .5)",
-              effects: [
-                {
-                  on: "hover",
-                  style: {
-                    itemBackground: "rgba(0, 0, 0, .03)",
-                    itemOpacity: 1,
-                  },
-                },
-              ],
-            },
-          ]}
-        />
-        <ResponsiveLine
-          data={formattedData || []}
-          theme={{
-            axis: {
-              domain: {
-                line: {
-                  stroke: theme.palette.secondary.light,
-                },
-              },
-              legend: {
-                text: {
-                  fill: theme.palette.secondary.light,
-                },
-              },
-              ticks: {
-                line: {
-                  stroke: theme.palette.secondary.light,
-                  strokeWidth: 1,
-                },
-                text: {
-                  fill: theme.palette.secondary.light,
-                },
-              },
-            },
-            legends: {
-              text: {
-                fill: theme.palette.secondary.light,
-              },
-            },
-            tooltip: {
-              container: {
-                color: theme.palette.primary.main,
-              },
-            },
-          }}
-          colors={{ datum: "color" }}
-          margin={{ top: 50, right: 50, bottom: 70, left: 60 }}
-          xScale={{ type: "point" }}
-          yScale={{
-            type: "linear",
-            min: "auto",
-            max: "auto",
-            stacked: false,
-            reverse: false,
-          }}
-          yFormat=" >-.2f"
-          curve="catmullRom"
-          axisTop={null}
-          axisRight={null}
-          axisBottom={{
-            //   orient: "bottom",
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 90,
-            legend: "Month",
-            legendOffset: 60,
-            legendPosition: "middle",
-          }}
-          axisLeft={{
-            //   orient: "left",
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "Total",
-            legendOffset: -50,
-            legendPosition: "middle",
-          }}
-          enableGridX={false}
-          enableGridY={false}
-          pointSize={10}
-          pointColor={{ theme: "background" }}
-          pointBorderWidth={2}
-          pointBorderColor={{ from: "serieColor" }}
-          pointLabelYOffset={-12}
-          useMesh={true}
-          legends={[
-            {
-              anchor: "top-right",
-              direction: "column",
-              justify: false,
-              translateX: 50,
+              translateX: 0,
               translateY: 0,
               itemsSpacing: 0,
               itemDirection: "left-to-right",
